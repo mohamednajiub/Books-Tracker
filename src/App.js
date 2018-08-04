82eeaac6e51e118f8c2470aeb8450693f1978959
Mohamed Emad <m.emad550@gmail.com>
2018-08-04 23:24:33 +0200

    feat: connect book component with bookLibrary component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import { Route } from "react-router-dom";
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import * as BooksAPI from './BooksAPI'
 import  BookLibrary  from './BookLibrary';
 import  SearchBook  from './SearchBook';
-
+import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+  state= {
+    books: []
   }
-
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
   render() {
+    console.log(this.state.books)
     return (
       <div className="app">          
-        <Route exact path='/' render={()=> <BookLibrary/>}/>
+        <Route exact path='/' render={()=> <BookLibrary books={this.state.books}/>}/>
         <Route path='/search' render={ () => <SearchBook/> }/>
       </div>
     )
